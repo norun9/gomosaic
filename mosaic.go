26d7fcde5dbfccf4f3f3785ae5b13ff9bb36d5ed
tomoyaueno29 <u.t012807@gmail.com>
2020-06-19 15:21:48 +0900

    Update mosaic.go

--- mosaic.go
+++ mosaic.go
@@ -1,1 +1,11 @@
-package GoMosaic
+package main
+
+import "image"
+
+func averageColor(img image.Image) [3]float64 {
+	bounds := img.Bounds()
+	r, g, b := 0.0, 0.0, 0.0
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		
+	}
+}
