11f6ed8f4a54a67ded8d7e2f2585bc1c3b9be53e
tomoyaueno29 <u.t012807@gmail.com>
2020-06-19 16:25:11 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	mux.HandleFunc("/", upload)
+	mux.HandleFunc("/mosaic", mosaic)
+
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+		Handler: mux,
+	}
+	TITLEDB = tilesDB()
+	fmt.Println("Mosaic server started")
+	server.ListenAndServe()
 }
